de6f14a663cfeb4fe0b7861e3f0f4afb848342cc
annika02 <at621504@gmail.com>
2025-05-22 02:51:11 +0600

    Refactor review endpoints and implement admin verification for review management

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+// const Stripe = require('stripe');
+// const stripe = Stripe(process.env.STRIPE_SC_KEY);
 
 // Middleware
 app.use(cors());
@@ -27,7 +29,61 @@ async function run() {
 
     const database = client.db("ScholarshipHub");
     const scholarshipsCollection = database.collection("Scholarships");
+    const userCollection = database.collection("Users");
+    const reviewCollection = database.collection("Reviews");
 
+    // use verify admin after verifyToken
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    const verifyModerator = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isModerator = user?.role === "moderator";
+      if (!isModerator) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    //////// user related ////////
+
+    app.post("/create-user", async (req, res) => {
+      const data = req.body;
+      const query = { userEmail: data.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne({
+        userName: data.displayName,
+        userEmail: data.email,
+        role: "user",
+      });
+      res.send(result);
+    });
+    //////// user related ////////
+
+    app.get("/users/:email", async (req, res) => {
+      const result = await userCollection.findOne({
+        userEmail: req.params.email,
+      });
+      res.send(result);
+    });
+
+    //////// data related ////////
+
+    // get data for home page condition => lowest application fee and recently added
     app.get("/", async (req, res) => {
       const result = await scholarshipsCollection
         .find()
@@ -38,14 +94,46 @@ async function run() {
       res.send(result);
     });
 
+    // get all data
     app.get("/all-data", async (req, res) => {
       const result = await scholarshipsCollection.find().toArray();
       res.send(result);
     });
     app.get("/scholarship/:id", async (req, res) => {
-      const result = await scholarshipsCollection.findOne({
-        _id: new ObjectId(req.params.id),
+      const result = await scholarshipsCollection
+        .aggregate([
+          {
+            $match: { _id: new ObjectId(req.params.id) },
+          },
+          {
+            $lookup: {
+              from: "Reviews",
+              localField: "_id",
+              foreignField: "postId",
+              pipeline: [{ $sort: { _id: -1 } }],
+              as: "reviews",
+            },
+          },
+        ])
+        .toArray();
+      res.send(result);
+    });
+
+    //////// review related ////////
+
+    app.post("/add-review/:id", async (req, res) => {
+      const id = req.params.id;
+      const review = req.body;
+      const result = await reviewCollection.insertOne({
+        postId: new ObjectId(id),
+        ...review,
       });
+      res.send(result);
+    });
+
+    app.get("/get-reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await reviewCollection.find({ postId: id }).toArray();
       res.send(result);
     });
 
